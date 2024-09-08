41c439b0ab20d448ecf13f1264d81d011976c542
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2024-09-08 03:01:36 -0500

    Better algorithm for Day 24 (adds Held-Karp to lib)

--- aoc_2016_24.py
+++ aoc_2016_24.py
@@ -1,22 +1,23 @@
 from functools import partial
-from itertools import pairwise, permutations
+from itertools import pairwise
 
 import mrm.ansi_term as ansi
-from mrm.point import adj_ortho, grid_as_dict
 from mrm.dijkstra import dijkstra, Dictlike
+from mrm.point import adj_ortho, grid_as_dict
+from mrm.tsp import held_karp
 
 with open('data/aoc-2016-24.txt', encoding = 'utf-8') as f:
     dat = [x.strip('\n') for x in f.readlines()]
 
 def preprocess():
     nodes = grid_as_dict(dat, valid=lambda c: c in '.0123456789')
-    pts = [k for k, v in sorted(nodes.items(), key = lambda x: x[1]) if v != '.']
+    pts = {int(v): k for k, v in sorted(nodes.items(), key = lambda x: x[1]) if v != '.'}
 
     sp_w = {}
     sp_p = {}
-    for i, p in enumerate(pts):
+    for i, p in pts.items():
         w, p = dijkstra(Dictlike(partial(adj_ortho, constrain_pos=nodes)), start_point=p)
-        for j, q in enumerate(pts):
+        for j, q in pts.items():
             sp_w[(i, j)] = w[q]
             sp_p[(i, j)] = p[q]
 
@@ -29,8 +30,8 @@ def viz(grid, nodes, paths, min_path):
     for y in range(len(dat)):
         for x in range(len(dat[0])):
             pt = (x, y)
-            if pt in nodes:
-                print(ansi.red(str(nodes.index(pt))), end='')
+            if pt in nodes.values():
+                print(ansi.red(str(list(nodes.values()).index(pt))), end='')
             elif pt in path_pts:
                 print(ansi.green('*'), end='')
             elif pt in grid:
@@ -41,45 +42,25 @@ def viz(grid, nodes, paths, min_path):
 
 def part1(output = True):
     nodes, weights, paths, grid = preprocess()
-    n_pts = len(nodes)
 
-    min_dist = sum(weights.values())
-    min_path = None
-    for arr in permutations(range(1, n_pts), n_pts - 1):
-        tot = 0
-        for a, b in pairwise([0] + list(arr)):
-            tot += weights[(a, b)]
-        if tot < min_dist:
-            min_dist = tot
-            min_path = arr
+    min_dist, min_path = held_karp(nodes, weights, dont_loop = True, start_point = 0)
 
     if output:
         ansi.clear_screen()
-        lmp = [0] + list(min_path)
-        print('Optimal path:', lmp)
-        viz(grid, nodes, paths, lmp)
+        print('Optimal path:', min_path)
+        viz(grid, nodes, paths, min_path)
 
     return min_dist
 
 def part2(output = True):
     nodes, weights, paths, grid = preprocess()
-    n_pts = len(nodes)
 
-    min_dist = sum(weights.values())
-    min_path = None
-    for arr in permutations(range(1, n_pts), n_pts - 1):
-        tot = 0
-        for a, b in pairwise([0] + list(arr) + [0]):
-            tot += weights[(a, b)]
-        if tot < min_dist:
-            min_dist = tot
-            min_path = arr
+    min_dist, min_path = held_karp(nodes, weights)
 
     if output:
         ansi.clear_screen()
-        lmp = [0] + list(min_path) + [0]
-        print('Optimal path:', lmp)
-        viz(grid, nodes, paths, lmp)
+        print('Optimal path:', min_path)
+        viz(grid, nodes, paths, min_path)
 
     return min_dist
 
